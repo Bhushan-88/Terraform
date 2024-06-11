e56be7c49be0f6631ca1273f8ff35ef52ca220b7
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-12 00:10:59 +0530

    added aws_instance

---
+++ aws_instance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "ap-northeast-1"
+    }
+
+resource "aws_instance" "terraform" {
+    ami = "ami-0f9fe1d9214628296"
+    instance_type = "t2.micro"
+    key_name = "tokyo"
+    tags = {
+        env = "dev"
+        name = "instance-1"
+    }
+vpc_security_group_ids = ["sg-0048d0b71a853da0b"]
+
+}
--- first_teraform_script.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "ap-northeast-1"
-    profile = "devops.tf"
-    }
-
-resource "aws_iam_user" "demo" {
-    name = "user1"
-}
