25a2f0f29b92ca6e06bfac673593a633738c0b38
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-13 16:46:54 +0530

    --- Auto Git Commit ---

--- aws_instance.tf
+++ aws_instance.tf
@@ -3,16 +3,17 @@ provider "aws" {
     }
 
 resource "aws_instance" "terraform" {
-    ami = "ami-0f9fe1d9214628296"
+    ami = "ami-067deadd431782242"
     instance_type = "t2.micro"
     key_name = "tokyo"
     tags = {
         env = "dev"
         name = "instance-1"
     }
-vpc_security_group_ids = ["sg-0048d0b71a853da0b"]
-
+vpc_security_group_id = ["sg-0c8782daf72c3f29d"]
 }
+
 variable "region" {
+
   
 }
