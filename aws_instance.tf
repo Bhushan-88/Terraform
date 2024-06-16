1e3c1d87f72db7e01542e4b072903a8b818d0122
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-16 14:28:46 +0530

    --- Auto Git Commit ---

--- aws_instance.tf
+++ aws_instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance-1" {
   ami = "ami-033fabdd332044f06"
   instance_type = "t2.micro"
   key_name = "ohio"
-  vpc_security_group_ids = "vpc-00a98d3275ef28193 "
+  vpc_security_group_ids = "sg-054e743f8fcca7143"
   tags = {
     dev = "dev"
     name ="my-ins"
