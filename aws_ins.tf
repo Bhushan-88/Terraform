f9b627f1e9528613c55bc91104f9f536ea7d0022
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-16 17:32:12 +0530

    --- Auto Git Commit ---

--- aws_ins.tf
+++ aws_ins.tf
@@ -5,4 +5,8 @@ resource "aws_instance" "instance-1" {
   ami = "ami-033fabdd332044f06"
   instance_type = "t2.micro"
   key_name = "ohio"
+  tags = {
+    env ="dev"
+    name = "instance-1"
+  }
 }
