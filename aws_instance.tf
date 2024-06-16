3cba53ab09b49b61288f84c4cd8377e5e8185f10
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-16 15:20:25 +0530

    --- Auto Git Commit ---

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,13 +1,17 @@
 provider "aws" {
-    region = "us-east-2"
+    region = var.region
 }
 resource "aws_instance" "instance-1" {
   ami = "ami-033fabdd332044f06"
   instance_type = "t2.micro"
   key_name = "ohio"
+  security_groups = ["sg-08b442c8b6b7db597", "sg-054e743f8fcca7143"]
   
   tags = {
     dev = "dev"
     name ="my-ins"
   }
+}
+variable = "regions" {
+  description = "enter region"
 }
