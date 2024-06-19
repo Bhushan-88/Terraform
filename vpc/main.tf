a12ed74d5d0760043584de57d67e195283710ae1
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-19 14:00:54 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -49,7 +49,7 @@ resource "aws_internet_gateway" "my_igw" {
 
 #aws_default_route_table
 resource "aws_default_route_table" "main_rt" {
-    default_route_table_id = aws_vpc.main.default_route_table_id
+    default_route_table_id = aws_vpc.main_rt.default_route_table_id
     route = {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.my_igw.id
