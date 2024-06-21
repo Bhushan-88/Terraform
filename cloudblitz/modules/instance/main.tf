2eb45c0e7bdf70add301bc407fb987a0c151edf2
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-21 16:21:02 +0530

    --- Auto Git Commit ---

---
+++ cloudblitz/modules/instance/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "my_instance" {
+    count = var.count
+    ami = var.image_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.sg_ids
+    key_name = var.key_pair
+    subnet_id = var.subnet_id
+    
+    tags = {
+        Name = "${var.project}-instance"
+        env = var.env
+    }
+}
+
