0668fb64dc4481a3cd05ab1ca8322c58d818b2ed
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-21 17:41:33 +0530

    --- Auto Git Commit ---

---
+++ cloudblitz/modules/variable.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "cloudblitz"
+}
+
+variable "vpc_cidr" {
+    default = "10.10.0.0/16"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "private_cidr" {
+    default = "10.10.0.0/20"  
+}
+
+variable "public_cidr" {
+    default = "10.10.16.0/20"
+}
